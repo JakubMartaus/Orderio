437324df1fac6819c8054a6aacb791142e96e33e
Jakub Martaus <martaus.jakub@gmail.com>
2019-01-16 09:28:28 +0100

    V2.1

--- Orderio/LoginPage.xaml.cs
+++ Orderio/LoginPage.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Net.Http;
+using Newtonsoft.Json;
 namespace Orderio
 {
     /// <summary>
@@ -27,16 +28,60 @@ namespace Orderio
         }
         string jmeno = "martaja15";
         string heslo = "orderio123";
+
+        public async void Get()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync("https://student.sps-prosek.cz/~martaja15/Files/API/api.php");
+
+            string json = await response.Content.ReadAsStringAsync();
+            //dynamic c = JsonConvert.DeserializeObject(json);
+            List<Objects.User> c = JsonConvert.DeserializeObject<List<Objects.User>>(json);
+
+            //string text = c[0].Gender;
+
+            // test.Content = text;
+            int ctr = c.Count();
+            for (int i = 0; i < ctr; i++)
+            {
+                /*ApiObject api = new ApiObject();
+                api.ID = c[i].ID;
+                api.Name = c[i].Name;
+                api.Surname = c[i].Surname;
+                api.PIN = c[i].PIN;
+                api.Birthdate = c[i].Birthdate;
+                api.Gender = c[i].Gender;
+
+                listView.Items.Add(api);*/
+                if(username.Text == c[i].Username && password.Password == c[i].Password)
+                {
+                    this.NavigationService.Navigate(new UserPage());
+                }
+                else
+                {
+                    if (i == ctr)
+                    {
+                        MessageBox.Show("Špatné přihlašovací údaje");
+                    }
+
+                }
+
+            }
+
+
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (username.Text == jmeno && password.Password == heslo)
+            Get();
+           /* if (username.Text == jmeno && password.Password == heslo)
             {
                 this.NavigationService.Navigate(new UserPage());
             }
             else
             {
                 MessageBox.Show("Wrong name or password!");
-            }
+            }*/
         }
         private void Registrace_Clicked(object sender, MouseButtonEventArgs e)
         {
--- Orderio/RegistrationPage.xaml.cs
+++ Orderio/RegistrationPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Orderio
 {
@@ -24,10 +26,50 @@ namespace Orderio
         {
             InitializeComponent();
         }
+        string Username;
+        string Firstname;
+        string Lastname;
+        string Password;
+        string Confirm;
+ 
+        public async void Send()
+        {
+            // Vytvoření klienta
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://student.sps-prosek.cz/~martaja15/Files/API/api.php");
+            // Data, která se přidají k POST dotazu -> klíč je typu string a data jsou typu string
+            var keyValues = new List<KeyValuePair<string, string>>();
+            keyValues.Add(new KeyValuePair<string, string>("Username", Username));
+            keyValues.Add(new KeyValuePair<string, string>("Firstname", Firstname));
+            keyValues.Add(new KeyValuePair<string, string>("Lastname", Lastname));
+            keyValues.Add(new KeyValuePair<string, string>("Password", Password));
+            keyValues.Add(new KeyValuePair<string, string>("Overeni", "post"));
+            // Přidání dat do dotazu
+            request.Content = new FormUrlEncodedContent(keyValues);
+            // Zaslání POST dotazu
+            var response = await client.SendAsync(request);
+            // Získání odpovědi
+            string responseContent = await response.Content.ReadAsStringAsync();
+        }
+       
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Username = username.Text;
+            Firstname = firstname.Text;
+            Lastname = lastname.Text;
+            Password = password.Password;
+            Confirm = confirmpassword.Password;
 
+            if (Password == Confirm)
+            {
+                Send();
+                this.NavigationService.Navigate(new LoginPage());
+            }
+            else
+            {
+                MessageBox.Show("Hesla se neshodují.");
+            }
         }
     }
 }
